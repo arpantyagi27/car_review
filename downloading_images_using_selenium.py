24273321416857598c3056717b35fea861f3f06f
Arpan <at5318@srmist.edu.in>
2022-08-13 23:46:58 +0530

    Added code for downloading images using selenium

---
+++ downloading_images_using_selenium.py
@@ -0,0 +1,90 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+# What you enter here will be searched for in Google Images
+querys =[
+	"keywords"
+]
+#make a loop if there are more than one query 
+for query in querys:
+	# Creating a webdriver instance
+	driver = webdriver.Chrome('path to the location of the driver')
+	
+	driver.maximize_window()
+
+	# link of the website 
+	driver.get('https://images.google.com/')
+
+	# Finding the search box
+	box = driver.find_element_by_xpath('//**[@id="sbtc"]/div/div[2]/input')
+
+	# Type the search query in the search box
+
+	box.send_keys(query)
+
+	# Pressing enter
+	box.send_keys(Keys.ENTER)
+
+	# Function for scrolling to the bottom of Google
+	# Images results
+	def scroll_to_bottom():
+
+		last_height = driver.execute_script('\
+		return document.body.scrollHeight')
+
+		while True:
+			driver.execute_script('\
+			window.scrollTo(0,document.body.scrollHeight)')
+
+			# waiting for the results to load
+			# Increase the sleep time if your internet is slow
+			time.sleep(3)
+
+			new_height = driver.execute_script('\
+			return document.body.scrollHeight')
+
+			# click on "Show more results" (if exists)
+			try:
+				driver.find_element_by_css_selector(".YstHxe input").click()
+
+				# waiting for the results to load
+				time.sleep(3)
+
+			except:
+				pass
+
+			# checking if we have reached the bottom of the page
+			if new_height == last_height:
+				break
+
+			last_height = new_height
+
+
+	# Calling the function
+	# there is no need to use the scroll_to_bottom() function if no of images is small.
+	scroll_to_bottom()
+	# Loop to capture and save each image
+	for i in range(1, 500):
+
+		# range(1, n) will capture images 1 to n-1 of the search results
+		try:
+
+		# XPath of each image
+			img = driver.find_element_by_xpath(' '//*[@id="islrg"]/div[1]/div[' +str(i) + ']/a[1]/div[1]/img'')
+
+			# Enter the location of folder in which
+			# the images will be saved
+			img.screenshot(f'path of the location'+'name'+str(i)+'.png')
+			# Each new screenshot will automatically have its name updated
+
+			# Just to avoid unwanted errors
+			time.sleep(0.2)
+
+		except:
+			# if we can't find the XPath of an image,
+			# we skip to the next image
+			continue
+
+	# Finally, we close the driver
+	driver.close()
